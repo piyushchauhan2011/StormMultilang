7d9a30f70c0db30bd2251fd2de121824d6e373d7
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-08-07 16:04:52 +1000

    Error correction in python program

--- src/main/java/org/piyush/resources/call_log_creator_bolt.py
+++ src/main/java/org/piyush/resources/call_log_creator_bolt.py
@@ -2,9 +2,9 @@ import storm
 
 class CallLogCreatorBolt(storm.BasicBolt):
     def process(self, tup):
-        from = tup[0]
-        to = tup[1]
-        duration = tup[3]
+        from = tup.values[0]
+        to = tup.values[1]
+        duration = tup.values[2]
         storm.emit([from + " - " + to, duration])
 
 CallLogCreatorBolt().run()
