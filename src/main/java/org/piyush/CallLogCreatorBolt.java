6387fdd2dee667b8f654d3951fc05f43273f7098
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-08-07 16:33:13 +1000

    Reverting back

--- src/main/java/org/piyush/CallLogCreatorBolt.java
+++ src/main/java/org/piyush/CallLogCreatorBolt.java
@@ -2,9 +2,7 @@ package org.piyush;
 
 //import util packages
 
-import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.ShellBolt;
-import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.IRichBolt;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.tuple.Fields;
@@ -15,14 +13,10 @@ import java.util.Map;
 
 //Create a class CallLogCreatorBolt which implement IRichBolt interface
 public class CallLogCreatorBolt extends ShellBolt implements IRichBolt {
-    //Create instance for OutputCollector which collects and emits tuples to produce output
-    private OutputCollector collector;
 
-    public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-        this.collector = collector;
+    public CallLogCreatorBolt() {
+        super("python", "call_log_creator_bolt.py");
     }
-
-    public void cleanup() {}
 
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
         declarer.declare(new Fields("call", "duration"));
@@ -30,9 +24,5 @@ public class CallLogCreatorBolt extends ShellBolt implements IRichBolt {
 
     public Map<String, Object> getComponentConfiguration() {
         return null;
-    }
-
-    public CallLogCreatorBolt() {
-        super("python", "call_log_creator_bolt.py");
     }
 }
