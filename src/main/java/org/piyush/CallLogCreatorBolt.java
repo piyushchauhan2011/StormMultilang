999ba550673e981202960bda479d009652b2b939
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-08-07 16:36:48 +1000

    All java code

--- src/main/java/org/piyush/CallLogCreatorBolt.java
+++ src/main/java/org/piyush/CallLogCreatorBolt.java
@@ -2,20 +2,35 @@ package org.piyush;
 
 //import util packages
 
-import org.apache.storm.task.ShellBolt;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.IRichBolt;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 
 import java.util.Map;
 
 //import Storm IRichBolt package
 
 //Create a class CallLogCreatorBolt which implement IRichBolt interface
-public class CallLogCreatorBolt extends ShellBolt implements IRichBolt {
+public class CallLogCreatorBolt implements IRichBolt {
+    //Create instance for OutputCollector which collects and emits tuples to produce output
+    private OutputCollector collector;
 
-    public CallLogCreatorBolt() {
-        super("python", "call_log_creator_bolt.py");
+    public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    public void execute(Tuple tuple) {
+        String from = tuple.getString(0);
+        String to = tuple.getString(1);
+        Integer duration = tuple.getInteger(2);
+        collector.emit(new Values(from + " - " + to, duration));
+    }
+
+    public void cleanup() {
     }
 
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
